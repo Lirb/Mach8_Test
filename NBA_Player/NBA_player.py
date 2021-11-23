d6dad355fadb800d97ea3438aff1ee9b660d3705
Antonio José Moreno Saavedra <ajmorenos@unal.edu.co>
2021-11-23 12:09:24 -0500

    Defined __eq__ method to check casses where are created NBA_player_duo instances using two instances of the same NBA_player

--- NBA_Player/NBA_player.py
+++ NBA_Player/NBA_player.py
@@ -12,6 +12,9 @@ class NBA_player:
     def __repr__(self) -> str:
         return f"<first name: {self.__first_name} last name: {self.__last_name} height (inches):{self.__h_in}"
     
+    def __eq__(self, other: object) -> bool:
+        return self.__dict__ == other.__dict___
+    
     def getFirstName(self) -> str:
         return self.__first_name
     def getLastName(self) -> str:

1c4181840a8493a1af0115770d596f2a6d4ea545
Antonio José Moreno Saavedra <ajmorenos@unal.edu.co>
2021-11-24 11:53:26 -0500

    removed method setTotalHeight and were added totalheight argumento the __init__ method

--- user/user.py
+++ user/user.py
@@ -1,17 +1,30 @@
 from typing import List
 from NBA_Player import NBA_player_duo
+import NBA_Player
+from NBA_Player.NBA_player_DB import NBA_player_DB
 class User:
-    def __init__(self) -> None:
+    def __init__(self, total_height: float) -> None:
         self.__total_height = 0.0
         self.__query_result = []
-        
-    def setTotalHeight(self, total_height: float) -> None:
+        self.__DB = NBA_player_DB() 
         self.__total_height = total_height
-        
+                     
     def listQueryResults(self) -> str:
         str_result_list = ""
         for nba_duo in self.__query_result:
             str_result_list += str(nba_duo)
         return str_result_list
-    def query():
-        pass
+    
+    def query(self):        
+        for index, nba_player_1 in enumerate(self.__DB):
+            if index == 0:
+                NBA_player_space, initial_pos, _ = self.__DB.filter(self.__total_height - nba_player_1.getHeightInches())
+            else:
+                NBA_player_space, _, _ =  self.__DB.filter(self.__total_height - nba_player_1.getHeightInches())
+            for nba_player_2 in NBA_player_space:
+                self.__query_result.append(NBA_player_duo(nba_player_1,nba_player_2))
+            if index == initial_pos:
+                break
+            
+            
+                    

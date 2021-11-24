911c75f2ad6ac8029fd2a958b8e7e1f68f4fdb22
Antonio José Moreno Saavedra <ajmorenos@unal.edu.co>
2021-11-24 08:48:25 -0500

    Changed method filter and  __init__ to return only those player with the same height in inches gived as argument

--- NBA_Player/NBA_player_DB.py
+++ NBA_Player/NBA_player_DB.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 class NBA_player_DB:
     def __init__(self) -> None:
         self.__data = []
-        self.__height_pos = {}
+        self.__height_final_pos = {}
+        self.__height_init_pos = {}
         self.__load()
    
     def __load(self) -> None:
@@ -26,12 +27,14 @@ class NBA_player_DB:
         print("Hola")
         for index, nba_player in enumerate(self.__data):
             height_inches = nba_player.getHeightInches()
-            self.__height_pos[float(height_inches)] = index
-                           
+            self.__height_final_pos[float(height_inches)] = index
+            if not height_inches in self.__height_init_pos:
+                self.__height_init_pos[height_inches] = index            
                          
     def filter(self, height: float):
-        if not height in self.__height_pos:
+        if not height in self.__height_init_pos:
             return None
-        pos = self.__height_pos[height]
-        return self.__data[:pos]            
+        init_pos = self.__height_init_pos[height]
+        final_pos = self.__height_final_pos
+        return self.__data[init_pos:final_pos]            
       
